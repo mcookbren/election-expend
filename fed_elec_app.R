b6632b382da356d6dfbfe0108769d8cd4ebe3792
Meghan Cook <meghancook10@yahoo.com>
2019-03-09 18:28:14 -0800

    First Figure

--- fed_elec_app.R
+++ fed_elec_app.R
@@ -4,27 +4,66 @@
 #Data Source: Federal Election Commission
 
 library(shiny)
+library(RColorBrewer)
 
 # Define UI for application that draws a histogram
 ui <- navbarPage("2016 Election Expenditures",
                  tabPanel("Presidential Election"),
-                 tabPanel("Congressional Elections"),
-                 tabPanel("Donor Profiles"),
-                 tabPanel("Data Summary")
+                 sidebarLayout(
+                   sidebarPanel(
+                     
+                     #input candidate
+                     selectInput("candidate", 
+                                 "Select Presidential Candidate:", 
+                                 choices = c(Trump ="Trump", Clinton = "Clinton")
+                     ),
+                     
+                     #input support or opose
+                     radioButtons("support", 
+                       "Was Donation to Support or Oppose?", 
+                       choices = c(Support ="Support", Oppose = "Oppose")
+                     )
+                     ), #close sidebar panel
+                   
+                   mainPanel(
+                     
+                     plotOutput("elections")
+                     
+                   )
+                     
+                     
+
+                 )
                  )
 
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw a barplot
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'red', border = 'orange')
+   output$elections <- renderPlot({
+     
+     exp_sum_t10 <- exp_sum %>% 
+       filter(can_nam == input$candidate) %>% 
+       filter(sup_opp == input$support) %>% 
+       arrange(desc(sum_exp)) %>% 
+       slice(1:10)
+     
+     ggplot(data=exp_sum_t10, aes(x=spe_nam, y=sum_exp, fill=spe_nam)) +
+       geom_bar(stat='identity') +
+       theme(axis.text.x=element_blank()) +
+       ggtitle("Top 10 Largest Donors") +
+       xlab("Donor Name") +
+       ylab("Total Amount Donated (USD)") +
+       scale_fill_brewer(palette="Dark2")
+  
    })
+   
+   
+   
+   
+   
+   
+   
 }
 
 # Run the application 
